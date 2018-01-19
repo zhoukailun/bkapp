a22f0e8e23c8fc5c52771cd54eef1a6c662e838a
zhoukl <542827583@qq.com>
2018-01-19 15:10:03 +0800

    d

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -22,7 +22,7 @@ DATABASES = {
         'NAME': 'bkapp',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'zhoukl123',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   		   # 数据库HOST
+        'HOST': '101.127.248.186',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
