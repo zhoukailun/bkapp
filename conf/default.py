fc1afd01d5e1142cd628d2077ffcd47b92cb9991
zhoukailun <35447302+zhoukailun@users.noreply.github.com>
2018-11-09 14:58:05 +0800

    Update default.py

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,9 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'bkapp'
-APP_TOKEN = 'f76cb380-a14e-443f-bbdc-664fdb09437f'
+APP_TOKEN = 'e8c8b894-e597-4e84-89d6-95c3a92db540'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://paas.blueking.com'
+BK_PAAS_HOST = 'http://paas.bk.test2.garena.com:81'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
