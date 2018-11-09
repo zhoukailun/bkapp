1f2c7b07adcb833ea15d096b0556158cbedf478e
zhoukailun <35447302+zhoukailun@users.noreply.github.com>
2018-11-09 14:54:00 +0800

    Update settings_testing.py

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'bkapp',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'zhoukl123',                        # 你的数据库password
-        'HOST': '101.127.248.186',                   		   # 数据库HOST
+        'PASSWORD': 'kV1B)yOvhD',                        # 你的数据库password
+        'HOST': '10.38.0.16',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
