5615daf585689c710c5921a454c30074240bcec2
zhoukailun <35447302+zhoukailun@users.noreply.github.com>
2018-11-21 15:58:50 +0800

    Update settings_production.py

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'bkapp',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'zhoukl123',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   		   # 数据库HOST
+        'PASSWORD': 'kV1B)yOvhD',                        # 你的数据库password
+        'HOST': '10.38.0.7',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
